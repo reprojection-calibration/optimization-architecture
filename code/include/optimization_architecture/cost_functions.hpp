4545d84b4d346a9ed57b0771ce07618516bf8f36
reprojection-calibration <reprojection.calibration@gmail.com>
2025-06-11 20:38:23 +0200

    add factory logic

--- code/include/optimization_architecture/cost_functions.hpp
+++ code/include/optimization_architecture/cost_functions.hpp
@@ -1,14 +1,26 @@
 #pragma once
 
+#include <ceres/ceres.h>
+
 namespace reprojection_calibration::optimization_architecture {
 
 struct OneParameterCostFunction {
+  OneParameterCostFunction(double const data) : data_{data} {}
+
   template <typename T>
   bool operator()(const T *const x, T *const residual) const {
     residual[0] = 10.0 - x[0];
 
     return true;
   }
+
+  static ceres::CostFunction *Create(double const data) {
+    return new ceres::AutoDiffCostFunction<OneParameterCostFunction, 1, 1, 1>(
+        data);
+  }
+
+private:
+  double data_;
 };
 
 struct TwoParameterCostFunction {
--- code/src/cost_functions.cpp
+++ code/src/cost_functions.cpp
@@ -1,5 +1,7 @@
 #include "optimization_architecture/cost_functions.hpp"
 
+// NOTE: We can remove this file if the cost functions will be template only
+
 namespace reprojection_calibration::optimization_architecture {
 
 void DeleteXxx() {
