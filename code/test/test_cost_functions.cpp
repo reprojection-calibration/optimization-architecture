b167757435c5803e14e6379a3624a40e95fc4547
reprojection-calibration <reprojection.calibration@gmail.com>
2025-06-11 19:06:45 +0200

    TASK: Add testing infrastructure  (#3)
    * ADD: .clang-format file

---
+++ code/test/test_cost_functions.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+#include "optimization_architecture/cost_functions.hpp"
+
+using namespace reprojection_calibration::optimization_architecture;
+
+TEST(TestCostFunctions, DeleteDoesNothing) { EXPECT_NO_THROW(DeleteXxx()); }
