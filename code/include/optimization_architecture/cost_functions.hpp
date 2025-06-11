e88d5fc4cd70605093d1bfcdc26142ebe01cff98
reprojection-calibration <reprojection.calibration@gmail.com>
2025-06-11 20:21:41 +0200

    add two basic cost functions

--- code/include/optimization_architecture/cost_functions.hpp
+++ code/include/optimization_architecture/cost_functions.hpp
@@ -2,6 +2,22 @@
 
 namespace reprojection_calibration::optimization_architecture {
 
-void DeleteXxx();
+struct OneParameterCostFunction {
+  template <typename T>
+  bool operator()(const T *const x, T *const residual) const {
+    residual[0] = 10.0 - x[0];
+
+    return true;
+  }
+};
+
+struct TwoParameterCostFunction {
+  template <typename T>
+  bool operator()(const T *const x, T *const residual) const {
+    residual[0] = 10.0 - x[0] - x[1];
+
+    return true;
+  }
+};
 
 } // namespace reprojection_calibration::optimization_architecture
--- code/test/test_cost_functions.cpp
+++ code/test/test_cost_functions.cpp
@@ -4,4 +4,22 @@
 
 using namespace reprojection_calibration::optimization_architecture;
 
-TEST(TestCostFunctions, DeleteDoesNothing) { EXPECT_NO_THROW(DeleteXxx()); }
+TEST(TestCostFunctions, OneParameterCostFunction) {
+  OneParameterCostFunction const cost_function;
+
+  double const parameter{10.0};
+  double residual;
+  cost_function.operator()<double>(&parameter, &residual);
+
+  EXPECT_NEAR(residual, 0.0, 1e-6);
+}
+
+TEST(TestCostFunctions, TwoParameterCostFunction) {
+  TwoParameterCostFunction const cost_function;
+
+  std::array<double, 2> const parameters{6.0, 4.0};
+  double residual;
+  cost_function.operator()<double>(parameters.data(), &residual);
+
+  EXPECT_NEAR(residual, 0.0, 1e-6);
+}
