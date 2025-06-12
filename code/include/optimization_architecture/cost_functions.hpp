0a1dfe7a7a3f32fb41a4cb3a26240b5d9f930ab1
reprojection-calibration <reprojection.calibration@gmail.com>
2025-06-12 03:03:08 +0200

    TASK: Add ceres solver and basic cost functions (#4)
    * install ceres
    
    * add two basic cost functions
    
    * add factory logic
    
    * add create method and constructor with data
    
    * test the Create() method

--- code/include/optimization_architecture/cost_functions.hpp
+++ code/include/optimization_architecture/cost_functions.hpp
@@ -1,7 +1,43 @@
 #pragma once
 
+#include <ceres/ceres.h>
+
 namespace reprojection_calibration::optimization_architecture {
 
-void DeleteXxx();
+struct OneParameterCostFunction {
+  OneParameterCostFunction(double const data) : data_{data} {}
+
+  template <typename T>
+  bool operator()(const T *const x, T *const residual) const {
+    residual[0] = data_ - x[0];
+
+    return true;
+  }
+
+  static ceres::CostFunction *Create(double const data) {
+    return new ceres::AutoDiffCostFunction<OneParameterCostFunction, 1, 1>(
+        new OneParameterCostFunction(data));
+  }
+
+  double data_;
+};
+
+struct TwoParameterCostFunction {
+  TwoParameterCostFunction(double const data) : data_{data} {}
+
+  template <typename T>
+  bool operator()(const T *const x, T *const residual) const {
+    residual[0] = 10.0 - x[0] - x[1];
+
+    return true;
+  }
+
+  static ceres::CostFunction *Create(double const data) {
+    return new ceres::AutoDiffCostFunction<TwoParameterCostFunction, 1, 2>(
+        new TwoParameterCostFunction(data));
+  }
+
+  double data_;
+};
 
 } // namespace reprojection_calibration::optimization_architecture
