7c9745958ea0f1bcfe7bc63e7f072b9395f87aa1
reprojection-calibration <reprojection.calibration@gmail.com>
2025-06-12 21:25:10 +0200

    add optimization and two parameter test

--- code/include/optimization_architecture/optimizer_handler.hpp
+++ code/include/optimization_architecture/optimizer_handler.hpp
@@ -36,9 +36,15 @@ private:
 void OptimizerHandler(
     double const data, CameraParameters *const camera_parameters,
     std::function<ceres::CostFunction *(double)> cost_function_factory) {
-  static_cast<void>(data);
-  static_cast<void>(camera_parameters);
-  static_cast<void>(cost_function_factory);
+
+  ceres::Problem problem;
+  ceres::CostFunction *cost_function{cost_function_factory(data)};
+  problem.AddResidualBlock(cost_function, nullptr,
+                           camera_parameters->GetParameterPtr());
+
+  ceres::Solver::Options const options;
+  ceres::Solver::Summary summary;
+  ceres::Solve(options, &problem, &summary);
 }
 
 } // namespace reprojection_calibration::optimization_architecture
